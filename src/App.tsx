ca8ddc06b79f4bc0c4d35a5563743d430b4349dc
carlos <carlos8189.mk@gmail.com>
2024-09-26 16:02:08 -0300

    feat(header):added navigation and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from './components/header';
 
 export default function App() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
