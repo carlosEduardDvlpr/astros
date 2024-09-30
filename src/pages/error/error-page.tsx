efe901af98b12f62290132ed0c5cfc0c58e75585
carlos <carlos8189.mk@gmail.com>
2024-09-30 09:59:35 -0300

    fix(geral): adjusts

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function ErrorPage() {
   return (
     <main style={{ justifyContent: 'center' }}>
-      <Link to={'/planets/Sun'}>Voltar para home</Link>
+      <Link to={'/'}>Voltar para home</Link>
     </main>
   );
 }
