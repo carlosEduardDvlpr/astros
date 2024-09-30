54f48eb23e1d466ed3c76aa76880dc9410e663dc
carlos <carlos8189.mk@gmail.com>
2024-09-29 22:54:51 -0300

    fix(adjusts):adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/header';
-import Preload from './components/preload/preload';
-import React from 'react';
 
 export default function App() {
-  const [blocked, setBlocked] = React.useState(false);
-
-  React.useEffect(() => {
-    window.addEventListener('load', () =>
-      setTimeout(() => setBlocked(true), 1500),
-    );
-    return () => window.removeEventListener('load', () => setBlocked(true));
-  }, []);
-
   return (
     <>
       <Header />
       <main>
-        {!blocked ? <Preload /> : ''}
         <Outlet />
       </main>
     </>
--- src/components/preload/preload.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './preload.module.css';
-
-export default function Preload() {
-  return (
-    <div className={`${styles.preload}`}>
-      <p>Carregando sua experiência ...</p>
-    </div>
-  );
-}
