7bec8c2a9305b02dbc36e02794943bc23635df6a
carlosEduardDvlpr <carlos8189.mk@gmail.com>
2024-10-03 12:46:56 -0300

    Update solar-system.tsx

--- src/components/models/solar-system.tsx
+++ src/components/models/solar-system.tsx
@@ -9,7 +9,7 @@ export function SolarSystemModel(props) {
   const navigate = useNavigate();
   const group = React.useRef();
   const { nodes, materials, animations } = useGLTF(
-    'https://github.com/carlosEduardDvlpr/astros/raw/refs/heads/master/public/models/solar-system.glb'
+    'https://impmjmvcachyhjcjnyjv.supabase.co/storage/v1/object/public/astros/solar-system.glb'
     );
   const { actions } = useAnimations(animations, group);
 
@@ -332,5 +332,5 @@ export function SolarSystemModel(props) {
 }
 
 useGLTF.preload(
-  'https://github.com/carlosEduardDvlpr/astros/raw/refs/heads/master/public/models/solar-system.glb'
+  'https://impmjmvcachyhjcjnyjv.supabase.co/storage/v1/object/public/astros/solar-system.glb'
 );
